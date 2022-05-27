5416b65989f522cf18e448f49e303fa3e7960685
juandacorrea112 <93919034+juandacorrea112@users.noreply.github.com>
2022-05-27 14:23:34 -0500

    se agrego

---
+++ 490-carrito-prev/products.js
@@ -0,0 +1,13 @@
+//productos del arreglo ==>
+const parts = [
+  {id:"part-01",product: 'rant',price: 180000,image: '/Images/rant.jpg'}, 
+  {id:"part-02",product: 'odyssey',price: 400000,image: '/Images/timon.jpg'}, 
+  {id:"part-03",product: 'cult',price: 700000,image: '/Images/cult.jpg'},
+  {id:"part-04",product: 'subrosa',price: 230000,image: '/Images/subrosa.jpg'}, 
+  {id:"part-05",product: 'mutanty',price: 500000,image: '/Images/mutanty.JPG'},
+  {id:"part-06",product: 'shadow',price: 170000,image: '/Images/platoshadow.jpg'},
+  {id:"part-07",product: 'federal',price: 280000,image: '/Images/federal.JPG'},
+  {id:"part-08",product: 'eclat',price: 300000,image: '/Images/eclat.jpeg'},
+  {id:"part-09",product: 'sunday',price: 110000,image: '/Images/codosunday.jpg'},
+  {id:"part-10",product: 'tall order',price: 3000000,image: '/Images/tallorderbmx.webp'},
+];
